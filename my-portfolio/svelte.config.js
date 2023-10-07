539808f6c30dcf0bf61d76f8e37aef0e8589b50d
Sam Elliott <soe1@uakron.edu>
2023-10-07 15:13:25 -0400

    going back

--- my-portfolio/svelte.config.js
+++ my-portfolio/svelte.config.js
@@ -1,12 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
-		}
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };
