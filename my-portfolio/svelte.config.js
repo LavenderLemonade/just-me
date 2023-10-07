9acfa2f7ead303d6d24ac3479d3aaf374ec93c35
Sam Elliott <soe1@uakron.edu>
2023-10-07 11:12:23 -0400

    reverting to 65e06ce

--- my-portfolio/svelte.config.js
+++ my-portfolio/svelte.config.js
@@ -1,13 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '200.html',
-		})
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };
